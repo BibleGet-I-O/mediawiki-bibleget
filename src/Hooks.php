7270b2f4f4d7fa9199abdd774198821f798c1c54
John R. D'Orazio <priest@johnromanodorazio.com>
2024-07-31 00:35:57 +0200

    fix phpcs linting

--- src/Hooks.php
+++ src/Hooks.php
@@ -6,55 +6,96 @@ use MediaWiki\Hook\ParserFirstCallInitHook;
 use Parser;
 use PPFrame;
 
-class Hooks implements ParserFirstCallInitHook
-{
-    public static function onParserFirstCallInit( Parser $parser ) {
-        $parser->setHook( 'biblequote', [ self:class, 'renderBibleQuoteTag' ] );
-        $parser->setFunctionHook( 'biblequote', [ self::class, 'renderBibleQuote' ] );
-    }
+class Hooks implements ParserFirstCallInitHook {
 
-    private static function retrieveBibleQuoteFromApi(string $bibleVersion, string $bibleQuote, string $hash) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://query.bibleget.io");
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch,CURLOPT_POSTFIELDS, "query={$BGET["ref"]}&version={$BGET["version"]}&appid=SeminaVerbi&return=html");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $output = curl_exec($ch);
-        curl_close($ch);
+	/**
+	 * @param Parser $parser
+	 */
+	public static function onParserFirstCallInit( Parser $parser ) {
+		$parser->setHook( 'biblequote', [ self::class, 'renderBibleQuoteTag' ] );
+		$parser->setFunctionHook( 'biblequote', [ self::class, 'renderBibleQuote' ] );
+	}
 
-        $output = str_replace(array("\n", "\r"), '', $output);
-        $output = preg_replace('/&lt;br( ){0,1}(\/){0,1}&gt;/','',$output);
-        $output = preg_replace('/&lt;(sm|pof|po|pol|pos|poif|poi|poil|po3|po3l|speaker|pr)&gt;/', '<span class="$1">', $output);
-        $output = preg_replace('/&lt;\/(sm|pof|po|pol|pos|poif|poi|poil|po3|po3l|speaker|pr)&gt;/', '</span>', $output);
-        $output = preg_replace('/&lt;(\/){0,1}i&gt;/', '<$1i>', $output);
+	/**
+	 * @param string $bibleVersion
+	 * @param string $bibleQuote
+	 * @param string $hash
+	 * @return string
+	 */
+	private static function retrieveBibleQuoteFromApi( string $bibleVersion, string $bibleQuote, string $hash ): string {
+		$ch = curl_init();
+		curl_setopt( $ch, CURLOPT_URL, "https://query.bibleget.io");
+		curl_setopt( $ch, CURLOPT_POST, true );
+		curl_setopt( $ch, CURLOPT_POSTFIELDS, "query={$bibleQuote}&version={$bibleVersion}&appid=SeminaVerbi&return=html" );
+		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+		$output = curl_exec( $ch );
+		curl_close( $ch );
 
-        file_put_contents("bibleQuotes/{$hash}.html",$output);
-        return $output;        
-    }
+		$output = str_replace(
+			[ "\n", "\r" ],
+			'',
+			$output );
+		$output = preg_replace(
+			'/&lt;br( ){0,1}(\/){0,1}&gt;/',
+			'',
+			$output
+		);
+		$output = preg_replace(
+			'/&lt;(sm|pof|po|pol|pos|poif|poi|poil|po3|po3l|speaker|pr)&gt;/',
+			'<span class="$1">',
+			$output
+		);
+		$output = preg_replace(
+			'/&lt;\/(sm|pof|po|pol|pos|poif|poi|poil|po3|po3l|speaker|pr)&gt;/',
+			'</span>',
+			$output
+		);
+		$output = preg_replace(
+			'/&lt;(\/){0,1}i&gt;/',
+			'<$1i>',
+			$output
+		);
 
-    private static function renderBibleQuoteTag( $input, array $args, Parser $parser, PPFrame $frame ) {
-        $bibleVersion = isset( $args['bibleVersion'] ) ? $args['bibleVersion'] : 'NABRE';
-        $bibleQuote = isset( $args['bibleQuote'] ) ? $args['bibleQuote'] : $input;
-        $str = $bibleVersion . "/" . $bibleQuote;
-        $tmp = preg_replace("/\s+/", "", $str);
-        $hash = md5($tmp);
-        if(file_exists("bibleQuotes/{$hash}.html") ) {
-            $html = file_get_contents("bibleQuotes/{$hash}.html");
-        } else {
-            $html = self::retrieveBibleQuoteFromApi($bibleVersion, $bibleQuote, $hash);
-        }
-        return [ $html, 'noparse' => true, 'isHTML' => true ];
-    }
+		file_put_contents( "bibleQuotes/{$hash}.html", $output );
+		return $output;
+	}
 
-    private static function renderBibleQuote( Parser $parser, $bibleVersion = 'NABRE', $bibleQuote = 'John3:16' ) {
-        $str = $bibleVersion . "/" . $bibleQuote;
-        $tmp = preg_replace("/\s+/", "", $str);
-        $hash = md5($tmp);
-        if(file_exists("bibleQuotes/{$hash}.html") ) {
-            $html = file_get_contents("bibleQuotes/{$hash}.html");
-        } else {
-            $html = self::retrieveBibleQuoteFromApi($bibleVersion, $bibleQuote, $hash);
-        }
-        return [ $html, 'noparse' => true, 'isHTML' => true ];
-    }
+	/**
+	 * @param string $input
+	 * @param array $args
+	 * @param Parser $parser
+	 * @param PPFrame $frame
+	 * @return array
+	 */
+	private static function renderBibleQuoteTag( string $input, array $args, Parser $parser, PPFrame $frame ): array {
+		$bibleVersion = isset( $args['bibleVersion'] ) ? $args['bibleVersion'] : 'NABRE';
+		$bibleQuote = isset( $args['bibleQuote'] ) ? $args['bibleQuote'] : $input;
+		$str = $bibleVersion . "/" . $bibleQuote;
+		$tmp = preg_replace( "/\s+/", "", $str );
+		$hash = md5($tmp);
+		if ( file_exists( "bibleQuotes/{$hash}.html" ) ) {
+			$html = file_get_contents( "bibleQuotes/{$hash}.html" );
+		} else {
+			$html = self::retrieveBibleQuoteFromApi( $bibleVersion, $bibleQuote, $hash );
+		}
+		return [ $html, 'noparse' => true, 'isHTML' => true ];
+	}
+
+	/**
+	 * @param Parser $parser
+	 * @param string $bibleVersion
+	 * @param string $bibleQuote
+	 * @return array
+	 */
+	private static function renderBibleQuote( Parser $parser, string $bibleVersion = 'NABRE', string $bibleQuote = 'John3:16' ): array {
+		$str = $bibleVersion . "/" . $bibleQuote;
+		$tmp = preg_replace( "/\s+/", "", $str );
+		$hash = md5( $tmp );
+		if ( file_exists( "bibleQuotes/{$hash}.html" ) ) {
+			$html = file_get_contents( "bibleQuotes/{$hash}.html" );
+		} else {
+			$html = self::retrieveBibleQuoteFromApi( $bibleVersion, $bibleQuote, $hash );
+		}
+		return [ $html, 'noparse' => true, 'isHTML' => true ];
+	}
 }
