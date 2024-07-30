16de9255fd56e5412644fc9722ce07dc016acc61
John R. D'Orazio <priest@johnromanodorazio.com>
2024-07-31 01:50:42 +0200

    define our magic word

---
+++ BibleGet.i18n.magic.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Internationalization file for the magic words of the BibleGet extension.
+ *
+ * @file BibleGet.i18n.magic.php
+ * @ingroup BibleGet
+ * @author JohnRDOrazio
+ */
+
+$magicWords = [];
+
+/**
+ * English
+ * @author JohnRDOrazio
+ */
+$magicWords['en'] = [
+	'biblequote' => [ 0, 'biblequote' ]
+];
+
+/**
+ * French
+ * @author JohnRDOrazio
+ */
+$magicWords['fr'] = [
+	'biblequote' => [ 0, 'citationbiblique' ]
+];
+
+/**
+ * German
+ * @author JohnRDOrazio
+ */
+$magicWords['de'] = [
+	'biblequote' => [ 0, 'bibelzitat' ]
+];
+
+/**
+ * Portuguese
+ * @author JohnRDOrazio
+ */
+$magicWords['pt'] = [
+	'biblequote' => [ 0, 'citaçãobíblica' ]
+];
+
+/**
+ * Spanish
+ * @author JohnRDOrazio
+ */
+$magicWords['es'] = [
+	'biblequote' => [ 0, 'citabíblica' ]
+];
